26fd7f75f3b42f7f2084a042aaf3bd485e7f1c9d
David Cristofaro <david@dtcristo.com>
2016-10-03 00:23:24 +1100

    Remove Dict from anagram solution

--- elm/anagram/Anagram.elm
+++ elm/anagram/Anagram.elm
@@ -1,6 +1,5 @@
 module Anagram exposing (detect)
 
-import Dict exposing (Dict)
 import List
 import String
 
@@ -8,7 +7,7 @@ import String
 detect : String -> List String -> List String
 detect word =
     List.filter (notEquivalent word)
-        >> List.filter (matchesDict (charDict word))
+        >> List.filter (isAnagram word)
 
 
 notEquivalent : String -> String -> Bool
@@ -16,26 +15,13 @@ notEquivalent a b =
     String.toLower a /= String.toLower b
 
 
-matchesDict : Dict Char Int -> String -> Bool
-matchesDict dict word =
-    dict == charDict word
+isAnagram : String -> String -> Bool
+isAnagram a b =
+    sortChars a == sortChars b
 
 
-charDict : String -> Dict Char Int
-charDict word =
-    String.foldl countChar Dict.empty (String.toLower word)
-
-
-countChar : Char -> Dict Char Int -> Dict Char Int
-countChar char =
-    Dict.update char increment
-
-
-increment : Maybe Int -> Maybe Int
-increment maybe =
-    case maybe of
-        Just integer ->
-            Just (integer + 1)
-
-        Nothing ->
-            Just 1
+sortChars : String -> List Char
+sortChars =
+    String.toLower
+        >> String.toList
+        >> List.sort
