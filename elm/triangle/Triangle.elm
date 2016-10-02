cbd386a41bd7d8fdba402b20300421ce8ba9afc6
David Cristofaro <david@dtcristo.com>
2016-10-02 22:35:50 +1100

    Simplify triangle solution

--- elm/triangle/Triangle.elm
+++ elm/triangle/Triangle.elm
@@ -13,17 +13,11 @@ triangleKind : number -> number -> number -> Result String Triangle
 triangleKind a b c =
     if a <= 0 || b <= 0 || c <= 0 then
         Err "Invalid lengths"
+    else if a + b <= c || b + c <= a || a + c <= b then
+        Err "Violates inequality"
     else if a == b && b == c && a == c then
         Ok Equilateral
+    else if a == b || b == c || a == c then
+        Ok Isosceles
     else
-        case List.sort [ a, b, c ] of
-            x :: y :: z :: [] ->
-                if z > x + y then
-                    Err "Violates inequality"
-                else if x == y || y == z then
-                    Ok Isosceles
-                else
-                    Ok Scalene
-
-            _ ->
-                Err "Invalid case"
+        Ok Scalene
