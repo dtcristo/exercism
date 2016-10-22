5557479adf06cbb0c813a5bf2699aa0b65aa4013
David Cristofaro <david@dtcristo.com>
2016-10-23 00:06:56 +1100

    Update strain

--- elm/strain/Strain.elm
+++ elm/strain/Strain.elm
@@ -21,17 +21,4 @@ keep' f list acc =
 
 discard : (a -> Bool) -> List a -> List a
 discard f list =
-    discard' f list []
-
-
-discard' : (a -> Bool) -> List a -> List a -> List a
-discard' f list acc =
-    case list of
-        [] ->
-            acc
-
-        x :: xs ->
-            if (f x) then
-                discard' f xs acc
-            else
-                discard' f xs (acc ++ [ x ])
+    keep (f >> not) list
