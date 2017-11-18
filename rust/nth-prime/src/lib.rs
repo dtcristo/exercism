65488dc52ad79d658fb8e9ba29b2c9f281d09a7d
David Cristofaro <david@dtcristo.com>
2017-11-18 23:43:33 +1100

    Add updated primality test

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -26,3 +26,22 @@ fn is_prime(n: u64) -> bool {
     }
     return true;
 }
+
+// Primality test: https://en.wikipedia.org/wiki/Primality_test
+//fn is_prime(n: u64) -> bool {
+//    if n < 1 {
+//        return false;
+//    } else if n < 3 {
+//        return true;
+//    } else if (n % 2 == 0) || (n % 3 == 0) {
+//        return false;
+//    }
+//    let mut i = 5;
+//    while i * i < n {
+//        if (n % i == 0) || (n % (i + 2) == 0) {
+//            return false;
+//        }
+//        i += 6;
+//    }
+//    return true;
+//}
