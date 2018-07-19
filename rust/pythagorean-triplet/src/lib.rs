6941445a4f2819844f6f1a1e17eac2b7593c9c8c
David Cristofaro <david@dtcristo.com>
2018-07-19 21:52:20 +1000

    Solve simultaneous equations

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,20 +1,11 @@
 pub fn find() -> Option<u32> {
-    for a in 1..998 {
-        for b in 1..998 {
-            for c in 1..998 {
-                if sums_to_1000(a, b, c) && is_triplet(a, b, c) {
-                    return Some(a * b * c);
-                }
-            }
+    for a in 1..1000 {
+        let a = a as f32;
+        let b = (1000f32 * (a - 500f32)) / (a - 1000f32);
+        let c = 1000f32 - a - b;
+        if b > 0f32 && c > 0f32 && b.round() == b && c.round() == c {
+            return Some((a * b * c) as u32);
         }
     }
     None
-}
-
-fn sums_to_1000(a: u32, b: u32, c: u32) -> bool {
-    a + b + c == 1000
-}
-
-fn is_triplet(a: u32, b: u32, c: u32) -> bool {
-    a * a + b * b == c * c
 }
