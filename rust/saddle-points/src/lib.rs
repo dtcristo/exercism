1b3b48dfe67c454fe95c490ad9f209b802506b81
David Cristofaro <david@dtcristo.com>
2019-01-06 16:07:12 +1100

    Fix variable names

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,22 +1,22 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let mut results = Vec::new();
-    for c in 0..input.len() {
-        for r in 0..input[c].len() {
-            if is_saddle_point(input, r, c) {
-                results.push((c, r));
+    for i in 0..input.len() {
+        for j in 0..input[i].len() {
+            if is_saddle_point(input, i, j) {
+                results.push((i, j));
             }
         }
     }
     results
 }
 
-fn is_saddle_point(input: &[Vec<u64>], row: usize, column: usize) -> bool {
-    let value = input[column][row];
-    let row_max = input[column].iter().max();
+fn is_saddle_point(input: &[Vec<u64>], row_index: usize, column_index: usize) -> bool {
+    let value = input[row_index][column_index];
+    let row_max = input[row_index].iter().max();
     if Some(&value) != row_max {
         return false
     }
-    let column_min = input.iter().map(|x| x[row]).min();
+    let column_min = input.iter().map(|x| x[column_index]).min();
     if Some(value) != column_min {
         return false
     }
